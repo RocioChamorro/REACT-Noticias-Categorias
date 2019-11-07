07460d9805d830eb9be23b2a05a56b3922fa4f8c
RocioChamorro <chamorrorocio5@gmail.com>
2019-11-06 20:36:43 -0500

    Updates

---
+++ precache-manifest.c909bcf89fbfaad4b62808e3f8427da0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2592d8146fdb6663184aaaa7f7d9f49",
+    "url": "/REACT-Noticias-Categorias.git/index.html"
+  },
+  {
+    "revision": "6a04dcdfda450383dfab",
+    "url": "/REACT-Noticias-Categorias.git/static/css/main.ad7b9162.chunk.css"
+  },
+  {
+    "revision": "a61344e1512dd019d404",
+    "url": "/REACT-Noticias-Categorias.git/static/js/2.3556e409.chunk.js"
+  },
+  {
+    "revision": "6a04dcdfda450383dfab",
+    "url": "/REACT-Noticias-Categorias.git/static/js/main.9d5d123d.chunk.js"
+  },
+  {
+    "revision": "e22358598331f69f7fc8",
+    "url": "/REACT-Noticias-Categorias.git/static/js/runtime-main.dd1d8848.js"
+  }
+]);
