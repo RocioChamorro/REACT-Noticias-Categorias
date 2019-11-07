6429e110b8c40a56d7f17067dc1279fd66e71792
RocioChamorro <chamorrorocio5@gmail.com>
2019-11-06 20:59:40 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REACT-Noticias-Categorias.git/precache-manifest.c909bcf89fbfaad4b62808e3f8427da0.js"
+  "/REACT-Noticias-Categoriasn/precache-manifest.ffa59748869c7815e6fd6e86e9d4bddc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACT-Noticias-Categorias.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACT-Noticias-Categoriasn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
